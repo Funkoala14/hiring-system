d0ea0212996d7d7c95b2403ffccbce6e68617d3f
Tu Ho <72232937+tuho9428@users.noreply.github.com>
2024-10-15 21:15:21 -0700

    HRP-2 [Middleware] Added auth Middleware

---
+++ server/middlewares/authMiddleware.js
@@ -0,0 +1,25 @@
+import { verify } from 'jsonwebtoken';
+
+const jwtValidation = (req, res, next) => {
+
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({
+      message: "No token provided",
+    });
+  }
+
+  try {
+    const decoded = verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+
+    next();
+  } catch (error) {
+    return res.status(401).json({ error: "Not authorized, invalid token" });
+  }
+};
+
+
+export default {jwtValidation};
+
