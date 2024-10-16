42ec916aaeb69308d5ca8a782c4a8959757aa053
Tu Ho <72232937+tuho9428@users.noreply.github.com>
2024-10-15 19:13:18 -0700

    HRP-2 [Backend Dev] Fixed User model

--- server/models/User.js
+++ server/models/User.js
@@ -22,7 +22,7 @@ const userSchema = new Schema({
         required: true,
         minlength: 6,
     },
-    role:     { type: String, enum: ['Employee', 'HR'], default: 'user', required: true },
+    role:     { type: String, enum: ['Employee', 'HR'], default: 'Employee', required: true },
     housingAssignment: { type: Schema.Types.ObjectId, ref: 'House' },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
@@ -40,36 +40,5 @@ UserSchema.pre('save', async function (next) {
 UserSchema.methods.matchPassword = async function (password) {
     return await compare(password, this.password);
 };
-
-export default model('User', userSchema);
-// models/User.js
-
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const userSchema = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-        minlength: 1,
-        maxlength: 20,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        match: [/.+@.+\..+/, 'Please provide a valid email'],
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 6,
-    },
-    role:     { type: String, enum: ['Employee', 'HR'], default: 'user', required: true },
-    housingAssignment: { type: Schema.Types.ObjectId, ref: 'House' },
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now }
-});
 
 module.exports = mongoose.model('User', userSchema);
