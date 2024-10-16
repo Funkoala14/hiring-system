6fc0819831d2ef4b75b2a1c30e1ac3ba9aa38717
Tu Ho <72232937+tuho9428@users.noreply.github.com>
2024-10-15 19:02:53 -0700

    HRP-2 [Backend Dev] Added User model

---
+++ server/models/User.js
@@ -0,0 +1,44 @@
+// models/User.js
+
+import { Schema as _Schema, model } from 'mongoose';
+import { genSalt, hash, compare } from 'bcrypt';
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 1,
+        maxlength: 20,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Please provide a valid email'],
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+    },
+    role:     { type: String, enum: ['Employee', 'HR'], default: 'user', required: true },
+    housingAssignment: { type: Schema.Types.ObjectId, ref: 'House' },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now }
+});
+
+// Hashing password before saving
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        next();
+    }
+    const salt = await genSalt(10);
+    this.password = await hash(this.password, salt);
+});
+
+UserSchema.methods.matchPassword = async function (password) {
+    return await compare(password, this.password);
+};
+
+export default model('User', userSchema);
