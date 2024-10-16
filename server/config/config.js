1566c265b052bfcbeedebf214da0553faa25b95f
Tu Ho <72232937+tuho9428@users.noreply.github.com>
2024-10-15 18:54:52 -0700

    HRP-2 [DB config] Fix db config

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,11 @@
+
+
 const config = {
     PORT: process.env.PORT,
     MONGO_URI: process.env.MONGO_URI,
     JWT_SECRET: process.env.JWT_SECRET,
+    
+    
 };
-
+console.log('process.env.MONGO_URI', process.env.MONGO_URI);
 export default config;
