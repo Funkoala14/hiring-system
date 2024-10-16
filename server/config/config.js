90ebb46ee3337421b2edb44b3b0d17872ebfb8af
Tu Ho <72232937+tuho9428@users.noreply.github.com>
2024-10-15 18:48:18 -0700

    HRP-2 [Utils Function] Added generateJWTSecret.js

--- server/config/config.js
+++ server/config/config.js
@@ -2,6 +2,8 @@ const config = {
     PORT: process.env.PORT,
     MONGO_URI: process.env.MONGO_URI,
     JWT_SECRET: process.env.JWT_SECRET,
+    
+    
 };
-
+console.log('process.env.MONGO_URI', process.env.MONGO_URI);
 export default config;
