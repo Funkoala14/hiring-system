ea56b09b7f07136901813eda30765f66bc8b30d1
Tu Ho <72232937+tuho9428@users.noreply.github.com>
2024-10-15 21:00:53 -0700

    HRP-2 [Backend Dev] Updated DB connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,6 +3,11 @@ import config from './config.js';
 
 const { MONGO_URI } = config;
 
+if (!MONGO_URI) {
+    console.error('MONGO_URI is not defined in the environment variables.');
+    process.exit(1); // Exit the process if MONGO_URI is missing
+}
+
 mongoose.set('strictQuery', false);
 mongoose
     .connect(MONGO_URI)
