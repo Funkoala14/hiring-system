586e373065041ec4eb14e28ff2a6d43aee9501c5
Tu Ho <72232937+tuho9428@users.noreply.github.com>
2024-10-17 00:18:14 -0700

    HRP-2 [Frontend Dev] Edited Home page

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -3,21 +3,10 @@ import './Home.scss';
 import { getUser } from '../../services/userServices';
 
 function Home() {
-    const [user, setUser] = useState();
-
-    const fetchData = async () => {
-        const data = await getUser();
-        setUser(data);
-        console.log(data);
-    };
-
-    useEffect(() => {
-        fetchData();
-    }, []);
 
     return (
         <div className='Home'>
-            {user && <h1>{user.name}</h1>}
+
             <h1>Welcome to MyApp</h1>
             <p>This is the home page.</p>
         </div>
