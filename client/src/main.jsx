c9207d9026c34480d762bc3aeea39af2df226df8
Tu Ho <72232937+tuho9428@users.noreply.github.com>
2024-10-17 00:14:36 -0700

    HRP-2 [Frontend Dev] Wrapped app with PersistGate

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,21 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App.jsx';
-import './global.scss';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
+import "./global.scss";
+import { Provider } from "react-redux"; // Import Provider from react-redux
+import store, { persistor } from './store/store.js';  
+import ReactDOM from "react-dom";
+import router from "./routes";
+import { PersistGate } from "redux-persist/integration/react";
 
-createRoot(document.getElementById('root')).render(
-    <StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </StrictMode>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Provider store={store}>
+      {/* PersistGate delays rendering until rehydration is complete */}
+      <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </StrictMode>
 );
