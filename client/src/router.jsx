bb74ec8cc14cced5e1d882d89566c3b796362b4e
Tu Ho <72232937+tuho9428@users.noreply.github.com>
2024-10-17 16:17:29 -0700

    HRP-2 [Frontend Dev] Edited Routes

--- client/src/router.jsx
+++ client/src/router.jsx
@@ -3,23 +3,63 @@ import React, { Suspense, lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import MainLayout from '/layouts/MainLayout';
 import { SendLink } from './pages/Registration/SendRegistration';
+import Login from "./pages/Home/Login";
+import Dashboard from "./pages/Home/Dashboard";
+import EmployeeInfo from "./pages/Home/EmployeeInfo";
+import NotFound from "./pages/Home/NotFound";
+import PrivateRoute from "./components/PrivateRoute";
+import Forbidden from "./pages/Home/Forbidden";
 
-const Home = lazy(() => import('/pages/Home/Home'));
+const Home = lazy(() => import("/pages/Home/Home"));
 const RegistrationPage = lazy(() => import('/pages/Registration/Registration'));
 
 
 function AppRouter() {
-    return (
-        <MainLayout>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path='/' element={<Home />} />
-                    <Route path="/contact" element={<SendLink />} />
-                    <Route path="/register" element={<RegistrationPage />} />
-                </Routes>
-            </Suspense>
-        </MainLayout>
-    );
+  return (
+    <MainLayout>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          {/* Home Route */}
+          <Route path="/" element={<Home />} />
+
+          {/* SendLink Route */}
+          <Route path="/contact" element={<SendLink />} />
+
+          {/* RegistrationPage Route */}
+          <Route path="/register" element={<RegistrationPage />} />
+
+          {/* Login Route */}
+          <Route path="login" element={<Login />} />
+
+          {/* HR Dashboard Route (Protected) */}
+          <Route
+            path="hr/dashboard"
+            element={
+              <PrivateRoute allowedRoles={["HR"]}>
+                <Dashboard />
+              </PrivateRoute>
+            }
+          />
+
+          {/* Employee Personal Info Route (Protected) */}
+          <Route
+            path="employee/personal-info"
+            element={
+              <PrivateRoute allowedRoles={["Employee"]}>
+                <EmployeeInfo />
+              </PrivateRoute>
+            }
+          />
+
+          {/* Forbidden Route */}
+          <Route path="forbidden" element={<Forbidden />} />
+
+          {/* Catch-all route for undefined paths */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
+    </MainLayout>
+  );
 }
 
 export default AppRouter;
