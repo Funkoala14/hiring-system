58c471ccdea1ac458c21ba9bcccad9783bf4fe2b
Tu Ho <72232937+tuho9428@users.noreply.github.com>
2024-10-16 23:31:21 -0700

    HRP-2 [Backend Dev] Added user verify route

--- server/routers/UserRouter.js
+++ server/routers/UserRouter.js
@@ -1,7 +1,8 @@
 import { Router } from 'express';
 import User from '../controllers/UserController.js';
-const { login, logout } = User;
+const { login, logout, checkToken } = User;
 import validationMiddleware from '../middlewares/validationMiddleware.js';
+import jwtValidation from '../middlewares/authMiddleware.js';
 const { loginUserValidation, createUserValidation } = validationMiddleware;
 
 const userRouter = Router();
@@ -11,5 +12,8 @@ userRouter.post('/login', loginUserValidation, login);
 
 // User logout route
 userRouter.get('/logout', logout);
+
+// User verify token route
+userRouter.get('/verify', jwtValidation, checkToken);
 
 export default userRouter;
