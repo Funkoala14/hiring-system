37be822eaae17cfff7b8d65243724dd4e2194323
Tu Ho <72232937+tuho9428@users.noreply.github.com>
2024-10-17 00:14:36 -0700

    HRP-2 [Frontend Dev] Wrapped app with PersistGate

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
 import "./global.scss";
 import { Provider } from "react-redux"; // Import Provider from react-redux
 import store, { persistor } from './store/store.js';  
-
+import ReactDOM from "react-dom";
+import router from "./routes";
 import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")).render(
