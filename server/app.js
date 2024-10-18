8dbd89039699cbf39af462898e12a79f6633cc6e
ste519 <qianzi@umich.edu>
2024-10-18 13:01:09 -0400

    HRP-38 [Backend] fix typo and add urlencoded middleware

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import userRouter from './routers/UserRouter.js';
+import visaStatusRouter from './routers/VisaStatusRouter.js';
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
@@ -13,15 +14,17 @@ const __dirname = path.dirname(__filename);
 app.use(
     cors({
         origin: 'http://localhost:3000',
-        credential: true,
+        credentials: true,
     })
 );
 
 app.use(cookieParser());
 app.use(express.json());
 app.use(morgan(':method :url :status :response-time ms'));
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/v1/api/user', userRouter);
+app.use('/v1/api/visa', visaStatusRouter);
 
 app.all('*', (_req, res) => {
     return res.status(404).json({ message: 'API Not Found' });
