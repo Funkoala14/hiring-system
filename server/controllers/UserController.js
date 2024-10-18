a0b01a5552e5387f270ba3ea03f0cb05b8456b30
Tu Ho <72232937+tuho9428@users.noreply.github.com>
2024-10-17 19:07:48 -0700

    HRP-11 [Backend] Updated user register controller

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,65 +1,56 @@
 import User from '../models/User.js';
 import { compare } from 'bcrypt';
 import jsonwebtoken from 'jsonwebtoken';
-const { sign , verify } = jsonwebtoken;
+const { sign } = jsonwebtoken;
 import validator from 'validator';
 import Employee from '../models/Employee.js';
 const { escape } = validator;
 
 // Register new user
 export const register = async (req, res) => {
-    const { password } = req.body;  // Only password will be sent from the client
-    const { token } = req.query;  // Extract the token from the query string
+
     const sanitizedUsername = escape(req.body.username);
-
+    const sanitizedEmail = escape(req.body.email);
+    const { password } = req.body;
+  
     try {
-
-         // Verify and decode the JWT token
-         const decoded = verify(token, process.env.JWT_SECRET);
-         const email = decoded.email;  // Extract the email from the decoded token
-         const sanitizedEmail = escape(email);
-         console.log('sanitizedEmail',sanitizedEmail);
-         
-         
-        const existingUser = await Employee.findOne({
-            $or: [{ username: sanitizedUsername }, { email: sanitizedEmail }],
-        })
-            .lean()
-            .exec();
-
-        if (existingUser) {
-            return res.status(409).json({ message: 'Username or email already exists' });
-        }
-
-        const user = await Employee.create({
-            username: sanitizedUsername,
-            email: sanitizedEmail,
-            password,
-        });
-
-        const NewToken = sign(
-            {
-                id: user._id,
-                username: user.username,
-                email: user.email,
-                role: user.role,
-            },
-            process.env.JWT_SECRET,
-            { expiresIn: '1d' }
-        );
-
-        res.cookie('token', NewToken, {
-            httpOnly: true,
-            sameSite: 'Strict',
-            maxAge: 24 * 60 * 60 * 1000,
-        });
-
-        res.status(201).json({ message: 'User registered successfully', token: NewToken });
+      const existingUser = await User.findOne({
+        $or: [{ username: sanitizedUsername }, { email: sanitizedEmail }]
+      }).lean().exec();
+  
+      if (existingUser) {
+        return res.status(409).json({ message: 'Username or email already exists' });
+      }
+  
+      const user = await User.create({
+        username: sanitizedUsername,
+        email: sanitizedEmail,
+        password,
+      });
+  
+      const token = sign(
+        {
+          id: user._id,
+          username: user.username,
+          email: user.email,
+          role: user.role,
+        },
+        process.env.JWT_SECRET,
+        { expiresIn: '1d' }
+      );
+  
+      res.cookie('token', token, {
+        httpOnly: true,
+        sameSite: 'Strict',
+        maxAge: 24 * 60 * 60 * 1000,
+      });
+  
+      res.status(201).json({ message: 'User registered successfully', token });
     } catch (error) {
-        console.error(error);
-        return res.status(500).json({ message: 'Internal server error' });
+      console.error(error);
+      return res.status(500).json({ message: 'Internal server error' });
     }
-};
+  };
 
 // Login user
 export const login = async (req, res) => {
