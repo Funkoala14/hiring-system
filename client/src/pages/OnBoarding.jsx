4b7978e0a1df6806043306d96c83556c523108a3
Tu Ho <72232937+tuho9428@users.noreply.github.com>
2024-10-19 02:56:57 -0700

    HRP-10 [Frontend] Fixed onboardingStatus redirect logic

--- client/src/pages/OnBoarding.jsx
+++ client/src/pages/OnBoarding.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 
 const OnBoarding = () => {
   return (
-    <h1>OnBoarding Page</h1>
+    <>
+      <h1>Onboarding Page</h1>
+      <p>Your status is either 'Not Started', 'Pending', or 'Rejected'.</p>
+
+    </>
+
   )
 }
 
