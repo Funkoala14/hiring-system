1f82277728326ce2bc90cd95ac91228b854ea986
susie-laptop <sishi1461@gmail.com>
2024-10-21 13:59:42 -0400

    HRP-47 [Backend]Add/delete house api

--- server/controllers/HousingController.js
+++ server/controllers/HousingController.js
@@ -1,10 +1,14 @@
+import { isObjectIdOrHexString,  ObjectId, Types } from "mongoose";
 import House from "../models/House.js";
 
 export const getHousesList = async (_req, res) => {
     try {
         const houses = await House.find()
             .select("-__v")
-            .populate({ path: "residents", select: "_id username firstName preferedName lastName phone email" }, { path: "facilityReports"})
+            .populate(
+                { path: "residents", select: "_id username firstName preferedName lastName phone email" },
+                { path: "facilityReports" }
+            )
             .lean()
             .exec();
 
@@ -12,4 +16,52 @@ export const getHousesList = async (_req, res) => {
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
+};
+
+export const postAddHouse = async (req, res) => {
+    try {
+        const { address, landlord } = req.body;
+
+        const newHouse = new House({ address, landlord });
+        await newHouse.save();
+
+        const houses = await House.find()
+            .select("-__v")
+            .populate({ path: "residents", select: "_id username firstName preferedName lastName phone email" })
+            .populate({ path: "facilityReports", select: "-__v" })
+            .lean()
+            .exec();
+
+        res.status(201).send({ message: "House added successfully", code: 201, data: houses });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+export const deleteHouse = async (req, res) => {
+    const { houseId } = req.body;
+    if (!isObjectIdOrHexString(houseId)) {
+        return res.status(400).json({ message: 'Invalid house ID format' });
+    }
+
+    const id = new Types.ObjectId(houseId)
+    console.log(houseId, id);
+    
+    try {
+        const house = await House.findById(id).lean().exec();
+        if (!house) {
+            return res.status(404).json({ message: "House not found" });
+        }
+
+        await House.findByIdAndDelete(houseId);
+
+        const houses = await House.find()
+            .select("-__v")
+            .populate(
+                { path: "residents", select: "_id username firstName preferedName lastName phone email" }
+            ).populate({ path: "facilityReports", select: "-__v" })
+            .lean()
+            .exec();
+        return res.status(200).json({ message: "House deleted successfully", code: 200, data: houses });
+    } catch (error) {}
 };
--- server/routers/HousingRouter.js
+++ server/routers/HousingRouter.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { checkPermission, jwtValidation } from "../middlewares/authMiddleware.js";
-import { getHousesList } from "../controllers/HousingController.js";
+import { deleteHouse, getHousesList, postAddHouse } from "../controllers/HousingController.js";
+import { housingValidation } from "../middlewares/validationMiddleware.js";
 
 const housingRouter = Router();
-// , jwtValidation, checkPermission('HR')
-housingRouter.get('/list', getHousesList);
+
+housingRouter
+    .get("/list", jwtValidation, checkPermission("HR"),  getHousesList)
+    .post("/add", jwtValidation, checkPermission("HR"), housingValidation, postAddHouse)
+    .delete("/delete", jwtValidation, checkPermission("HR"), deleteHouse);
 
 export default housingRouter;
