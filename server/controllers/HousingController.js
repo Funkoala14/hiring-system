d29c175a2db4f366952dacff39c9b62ffb8bfe14
susie-laptop <sishi1461@gmail.com>
2024-10-21 14:22:04 -0400

    HRP-47 [Backend]fix housing list ppopulate bug

--- server/controllers/HousingController.js
+++ server/controllers/HousingController.js
@@ -1,14 +1,12 @@
-import { isObjectIdOrHexString,  ObjectId, Types } from "mongoose";
+import { isObjectIdOrHexString, ObjectId, Types } from "mongoose";
 import House from "../models/House.js";
 
 export const getHousesList = async (_req, res) => {
     try {
         const houses = await House.find()
             .select("-__v")
-            .populate(
-                { path: "residents", select: "_id username firstName preferedName lastName phone email" },
-                { path: "facilityReports" }
-            )
+            .populate({ path: "residents", select: "_id username firstName preferedName lastName phone email" })
+            .populate({ path: "facilityReports" })
             .lean()
             .exec();
 
@@ -41,12 +39,12 @@ export const postAddHouse = async (req, res) => {
 export const deleteHouse = async (req, res) => {
     const { houseId } = req.body;
     if (!isObjectIdOrHexString(houseId)) {
-        return res.status(400).json({ message: 'Invalid house ID format' });
+        return res.status(400).json({ message: "Invalid house ID format" });
     }
 
-    const id = new Types.ObjectId(houseId)
+    const id = new Types.ObjectId(houseId);
     console.log(houseId, id);
-    
+
     try {
         const house = await House.findById(id).lean().exec();
         if (!house) {
@@ -57,9 +55,8 @@ export const deleteHouse = async (req, res) => {
 
         const houses = await House.find()
             .select("-__v")
-            .populate(
-                { path: "residents", select: "_id username firstName preferedName lastName phone email" }
-            ).populate({ path: "facilityReports", select: "-__v" })
+            .populate({ path: "residents", select: "_id username firstName preferedName lastName phone email" })
+            .populate({ path: "facilityReports", select: "-__v" })
             .lean()
             .exec();
         return res.status(200).json({ message: "House deleted successfully", code: 200, data: houses });
