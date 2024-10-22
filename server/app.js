a5fbb4ab28deb29101d57b6e7eff600ac6068489
Sean Li <seangalaxies@gmail.com>
2024-10-22 11:19:06 -0700

    updated app.js for route

--- server/app.js
+++ server/app.js
@@ -10,6 +10,8 @@ import employeeRouter from './routers/EmployeeRouter.js';
 import newUserRouter from './routers/NewUserRouter.js';
 import dotenv from 'dotenv';
 import housingRouter from './routers/HousingRouter.js';
+import OnboardingApplicationRouter from './routers/OnboardingApplicationRouter.js';
+
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
@@ -37,6 +39,7 @@ app.use('/v1/api/user', newUserRouter);
 app.use('/v1/api/visa', visaStatusRouter);
 app.use('/v1/api/employee', employeeRouter);
 app.use('/v1/api/housing', housingRouter);
+app.use('/v1/api/onboarding', OnboardingApplicationRouter);
 
 app.all('*', (_req, res) => {
     return res.status(404).json({ message: 'API Not Found' });
