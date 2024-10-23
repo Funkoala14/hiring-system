5c04dbcdc64068f4b8f64d95e3c7db039c129457
Tu Ho <72232937+tuho9428@users.noreply.github.com>
2024-10-22 22:52:38 -0700

    HRP-10 [Backend] Added onboarding controller and router

--- server/app.js
+++ server/app.js
@@ -12,6 +12,7 @@ import dotenv from 'dotenv';
 import housingRouter from './routers/HousingRouter.js';
 import OnboardingApplicationRouter from './routers/OnboardingApplicationRouter.js';
 import fileRouter from './routers/fileRouter.js';
+import onboardingRouter from './routers/OnboardingRouter.js';
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
@@ -35,6 +36,7 @@ app.use('/v1/api/employee', employeeRouter);
 app.use('/v1/api/housing', housingRouter);
 app.use('/v1/api/onboarding', OnboardingApplicationRouter);
 app.use('/v1/api/file', fileRouter);
+app.use('/v1/api/onboarding', onboardingRouter);
 
 app.all('*', (_req, res) => {
     return res.status(404).json({ message: 'API Not Found' });
