e752f4a432e2878955533f548bcfe8cbcfd2a65a
Tu Ho <72232937+tuho9428@users.noreply.github.com>
2024-10-23 18:33:20 -0700

    HRP-10 [Backend] upload Multiple Files

--- server/middlewares/fileMiddleware.js
+++ server/middlewares/fileMiddleware.js
@@ -3,6 +3,7 @@ import config from '../config/config.js';
 import multer from 'multer';
 import multerS3 from 'multer-s3';
 
+// Initialize S3 client
 export const s3Client = new S3Client({
     region: config.AWS_REGION,
     credentials: {
@@ -11,15 +12,23 @@ export const s3Client = new S3Client({
     },
 });
 
+// Define multer storage using multerS3
 const upload = multer({
     storage: multerS3({
         s3: s3Client,
         bucket: config.AWS_S3_BUCKET_NAME,
         key: (req, file, cb) => {
-            cb(null, `${Date.now().toString()}-${file.originalname}`);
+            cb(null, `${Date.now()}-${file.originalname}`); // Use timestamp for unique filenames
         },
     }),
 });
 
-export const uploadFileMiddleware = upload.single('file');
+// Middleware to handle multiple file uploads
+export const uploadMultipleFilesMiddleware = upload.fields([
+    { name: 'profilePicture', maxCount: 1 },               // Profile image
+    { name: 'driverLicenseFile', maxCount: 1 },      // Driver license
+    { name: 'visaDocuments', maxCount: 5 },      // Visa documents (multiple files)
+]);
 
+// Single file upload for a generic field 'file'
+export const uploadFileMiddleware = upload.single('file');
