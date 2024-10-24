1e9fea899847681fa2b788784250adea20106bae
susie-laptop <sishi1461@gmail.com>
2024-10-24 13:51:57 -0400

    HRP-18 [test]format and navbar naming change

--- server/models/Employee.js
+++ server/models/Employee.js
@@ -1,10 +1,10 @@
 import { Schema } from 'mongoose';
 import validator from 'validator';
 import User from './User.js';
-import House from './House.js'
+import House from './House.js';
 import VisaStatus from './VisaStatus.js'; // Import to make sure VisaStatus has been initialized correctly
 import Application from './Application.js';
-import onboardingStatus from './OnboardingStatus.js'
+import onboardingStatus from './OnboardingStatus.js';
 
 const refType = Schema.Types.ObjectId;
 
@@ -22,65 +22,63 @@ const EmployeeSchema = new Schema({
     gender: { type: String, enum: ['male', 'female', 'other', ''], default: '' },
     cellPhone: { type: String, trim: true, default: '' },
     workPhone: { type: String, trim: true, default: '' },
-    emergencyContacts: [{
-        firstName: { type: String, trim: true, default: '' },
-        lastName: { type: String, trim: true, default: '' },
-        middleName: { type: String, trim: true, default: '' },
-        phone: { type: String, trim: true, default: '' },
-        email: { type: String, trim: true, default: '' },
-        relationship: { type: String, trim: true, default: '' },
-    }],
-    address: {
-        buildingOrAptNumber: { type: String, trim: true, default: "", maxlength: 10 },
-        street: { type: String, trim: true, default: "" },
-        city: { type: String, trim: true, default: "" },
-        state: { type: String, trim: true, default: "", maxlength: 2 },
-        zipCode: {
-            type: String,
-            trim: true,
-            default: '',
+    emergencyContacts: [
+        {
+            firstName: { type: String, trim: true, default: '' },
+            lastName: { type: String, trim: true, default: '' },
+            middleName: { type: String, trim: true, default: '' },
+            phone: { type: String, trim: true, default: '' },
+            email: { type: String, trim: true, default: '' },
+            relationship: { type: String, trim: true, default: '' },
         },
+    ],
+    address: {
+        buildingOrAptNumber: { type: String, trim: true, default: '', maxlength: 10 },
+        street: { type: String, trim: true, default: '' },
+        city: { type: String, trim: true, default: '' },
+        state: { type: String, trim: true, default: '', maxlength: 2 },
+        zipCode: { type: String, trim: true, default: '' },
     },
     housingAssignment: { type: refType, ref: 'House' },
     visaStatus: { type: refType, ref: 'VisaStatus' },
     onboardingStatus: { type: refType, ref: 'OnboardingStatus' },
     driverLicense: {
-        number: { type: String, trim: true, default: ""  },
-        expirationDate: { type: Date, default: null  },
-        copy: { type: String, trim: true, default: "" }, // Driver license file URL
-        copyName: { type: String, trim: true, default: "" }, // Driver license file URL
+        number: { type: String, trim: true, default: '' },
+        expirationDate: { type: Date, default: null },
+        copy: { type: String, trim: true, default: '' }, // Driver license file URL
+        copyName: { type: String, trim: true, default: '' }, // Driver license file URL
     },
     reference: {
-        firstName: { type: String, trim: true, default: ""},
-        lastName: { type: String, trim: true, default: "" },
-        phone: { type: String, trim: true, default: "" },
-        email: { type: String, trim: true, default: "" },
-        relationship: { type: String, trim: true, default: "" },
+        firstName: { type: String, trim: true, default: '' },
+        lastName: { type: String, trim: true, default: '' },
+        phone: { type: String, trim: true, default: '' },
+        email: { type: String, trim: true, default: '' },
+        relationship: { type: String, trim: true, default: '' },
     },
     carInfo: {
-        make: { type: String, trim: true, default: ""},
-        model: { type: String, trim: true, default: ""},
-        color: { type: String, trim: true, default: ""},
-      },
+        make: { type: String, trim: true, default: '' },
+        model: { type: String, trim: true, default: '' },
+        color: { type: String, trim: true, default: '' },
+    },
     onboardingStatus: { type: refType, ref: 'OnboardingStatus' },
     driverLicense: {
-        hasLicense:  { type: String },
-        number: { type: String, trim: true, default: ""  },
-        expirationDate: { type: Date, default: null  },
-        copy: { type: String, trim: true, default: "" }, // Driver license file URL
-        copyName: { type: String, trim: true, default: "" }, // Driver license file URL
+        hasLicense: { type: String },
+        number: { type: String, trim: true, default: '' },
+        expirationDate: { type: Date, default: null },
+        copy: { type: String, trim: true, default: '' }, // Driver license file URL
+        copyName: { type: String, trim: true, default: '' }, // Driver license file URL
     },
     reference: {
-        firstName: { type: String, trim: true, default: ""},
-        lastName: { type: String, trim: true, default: "" },
-        phone: { type: String, trim: true, default: "" },
-        email: { type: String, trim: true, default: "" },
-        relationship: { type: String, trim: true, default: "" },
+        firstName: { type: String, trim: true, default: '' },
+        lastName: { type: String, trim: true, default: '' },
+        phone: { type: String, trim: true, default: '' },
+        email: { type: String, trim: true, default: '' },
+        relationship: { type: String, trim: true, default: '' },
     },
     carInfo: {
-        make: { type: String, trim: true, default: ""},
-        model: { type: String, trim: true, default: ""},
-        color: { type: String, trim: true, default: ""},
+        make: { type: String, trim: true, default: '' },
+        model: { type: String, trim: true, default: '' },
+        color: { type: String, trim: true, default: '' },
     },
 });
 
