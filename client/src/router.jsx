d499d48066f5d7310cfdc547a841f160bd37b2de
Tu Ho <72232937+tuho9428@users.noreply.github.com>
2024-10-23 17:30:03 -0700

    HRP-10 [Backend] edited routers

--- client/src/router.jsx
+++ client/src/router.jsx
@@ -1,103 +1,74 @@
 import React, { Suspense, lazy } from "react";
 import { Routes, Route, Outlet } from "react-router-dom";
+import { SendLink } from "./pages/Registration/SendRegistration";
 import MainLayout from "/layouts/MainLayout";
 import VisaStatus from "./pages/VisaStatus/VisaStatus";
-import { SendLink } from "./pages/Registration/SendRegistration";
-import React, { Suspense, lazy } from "react";
-import { Routes, Route, Outlet } from "react-router-dom";
-import MainLayout from "/layouts/MainLayout";
-import VisaStatus from "./pages/VisaStatus/VisaStatus";
-import { SendLink } from "./pages/Registration/SendRegistration";
 import Login from "./pages/Home/Login";
 import Dashboard from "./pages/Home/Dashboard";
 import NotFound from "./pages/Home/NotFound";
 import PrivateRoute from "./components/PrivateRoute";
 import Forbidden from "./pages/Home/Forbidden";
-import OnBoarding from "./pages/OnBoarding";
+import OnBoarding from "./pages/OnBoarding/OnBoarding";
+import Confirmation from "./pages/OnBoarding/Confirmation";
 import Profile from "./pages/Profile/Profile";
 import EmployeeManagement from "./pages/EmployeeManagement/EmployeeManagement";
 import HousingManagement from "./pages/HousingManagement/HousingManagement";
 import HousingView from "./pages/HousingView/HousingView";
-import ApplicationTables from './pages/ViewOnboardingApplications';
-import ApplicationDetails from './pages/ApplicationDetails';
+import ApplicationTables from "./pages/ViewOnboardingApplications";
+import ApplicationDetails from "./pages/ApplicationDetails";
 
-const RegistrationPage = lazy(() => import('/pages/Registration/Registration'));
-
+const RegistrationPage = lazy(() => import("/pages/Registration/Registration"));
 
 const Home = lazy(() => import("/pages/Home/Home"));
 
 function AppRouter() {
-    return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <Routes>
-                {/* Home Route */}
-                <Route
-                    path='/'
-                    element={
-                        <MainLayout>
-                            <Home />
-                        </MainLayout>
-                    }
-                />
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        {/* Home Route */}
+        <Route
+          path="/"
+          element={
+            <MainLayout>
+              <Home />
+            </MainLayout>
+          }
+        />
 
-                {/* SendLink Route */}
-                <Route
-                    path='/contact'
-                    element={
-                        <MainLayout>
-                            <SendLink />
-                        </MainLayout>
-                    }
-                />
+        {/* SendLink Route */}
+        <Route
+          path="/contact"
+          element={
+            <MainLayout>
+              <SendLink />
+            </MainLayout>
+          }
+        />
 
-        {/* RegistrationPage Route */}
-        <Route path="/register" element={<RegistrationPage />} />
         {/* RegistrationPage Route */}
         <Route path="/register" element={<RegistrationPage />} />
 
         {/* Login Route */}
-        <Route path='/login' element={<Login />} />
+        <Route path="/login" element={<Login />} />
 
-                {/* Employee Routes */}
-                <Route
-                    path='employee'
-                    element={
-                        <PrivateRoute allowedRoles={["Employee"]}>
-                            <Outlet /> {/* Outlet to render nested routes */}
-                        </PrivateRoute>
-                    }
-                >
-                    {/* Employee Details Route (for onboarding status check) */}
-                    <Route
-                        path='details'
-                        element={<OnboardingStatus />} // OnboardingStatus will handle the redirect logic
-                    />
-                    {/* Employee Personal Info Route (with MainLayout) */}
-                    <Route
-                        path='my-profile'
-                        element={
-                            <MainLayout>
-                                <Profile parent={"employee"} />
-                            </MainLayout>
-                        }
-                    />
-                    <Route
-                        path='housing-detail'
-                        element={
-                            <MainLayout>
-                                <HousingView parent='employee' />
-                            </MainLayout>
-                        }
-                    />
         {/* Employee Routes */}
         <Route
           path="employee"
           element={
             <PrivateRoute allowedRoles={["Employee"]}>
-              <Outlet /> {/* Nested employee routes will be rendered here */}
+              <Outlet /> {/* Outlet to render nested routes */}
             </PrivateRoute>
           }
         >
+          {/* On-Boarding Route (without Header and Navbar) */}
+
+          <Route path="on-boarding" element={<OnBoarding />} />
+
+          {/* Confirmation Route */}
+          <Route
+            path="on-boarding/confirmation"
+            element={<Confirmation parent={"on-boarding"} />}
+          />
           {/* Employee Personal Info Route (with MainLayout) */}
           <Route
             path="my-profile"
@@ -107,106 +78,107 @@ function AppRouter() {
               </MainLayout>
             }
           />
-
-          {/* On-Boarding Route (without Header and Navbar) */}
-
-          <Route path="on-boarding" element={<OnBoarding/>} /> 
-
-          {/* Confirmation Route */}
           <Route
-            path="on-boarding/confirmation"
-            element={<Confirmation parent={"on-boarding"} />}
+            path="housing-detail"
+            element={
+              <MainLayout>
+                <HousingView parent="employee" />
+              </MainLayout>
+            }
           />
- 
+        </Route>
 
-                    {/* Visa-Status Route */}
-                    <Route
-                        path='visa-status'
-                        element={
-                            <MainLayout>
-                                <VisaStatus />
-                            </MainLayout>
-                        }
-                    />
-                </Route>
+        {/* HR Dashboard Route (Protected) */}
+        <Route
+          path="hr"
+          element={
+            <PrivateRoute allowedRoles={["HR"]}>
+              <Outlet />
+            </PrivateRoute>
+          }
+        >
+          <Route
+            path="dashboard"
+            element={
+              <MainLayout>
+                <Dashboard />
+              </MainLayout>
+            }
+          />
+          <Route
+            path="employee-management"
+            element={
+              <MainLayout>
+                <EmployeeManagement />
+              </MainLayout>
+            }
+          />
+          <Route
+            path="employee-profile"
+            element={
+              <MainLayout>
+                <Profile parent={"hr"} />
+              </MainLayout>
+            }
+          />
+          <Route
+            path="housing-mangement"
+            element={
+              <MainLayout>
+                <HousingManagement />
+              </MainLayout>
+            }
+          />
+          <Route
+            path="housing-detail"
+            element={
+              <MainLayout>
+                <HousingView parent="hr" />
+              </MainLayout>
+            }
+          />
 
-                {/* HR Dashboard Route (Protected) */}
-                <Route
-                    path='hr'
-                    element={
-                        <PrivateRoute allowedRoles={["HR"]}>
-                            <Outlet />
-                        </PrivateRoute>
-                    }
-                >
-                    <Route
-                        path='dashboard'
-                        element={
-                            <MainLayout>
-                                <Dashboard />
-                            </MainLayout>
-                        }
-                    />
-                    <Route
-                        path='employee-management'
-                        element={
-                            <MainLayout>
-                                <EmployeeManagement />
-                            </MainLayout>
-                        }
-                    />
-                    <Route
-                        path='employee-profile'
-                        element={
-                            <MainLayout>
-                                <Profile parent={"hr"} />
-                            </MainLayout>
-                        }
-                    />
-                    <Route
-                        path='housing-mangement'
-                        element={
-                            <MainLayout>
-                                <HousingManagement />
-                            </MainLayout>
-                        }
-                    />
-                    <Route
-                        path='housing-detail'
-                        element={
-                            <MainLayout>
-                                <HousingView parent='hr' />
-                            </MainLayout>
-                        }
-                    />
+          {/* Review Applications Route */}
+          <Route
+            path="onboarding-review"
+            element={
+              <MainLayout>
+                <ApplicationTables />
+              </MainLayout>
+            }
+          />
+          <Route
+            path="application/:id"
+            element={
+              <MainLayout>
+                <ApplicationDetails />
+              </MainLayout>
+            }
+          />
+        </Route>
 
-                    {/* Review Applications Route */}
-                    <Route path="onboarding-review" element={<MainLayout><ApplicationTables /></MainLayout>} />
-                    <Route path="application/:id" element={<MainLayout><ApplicationDetails /></MainLayout>} /> 
-                </Route>
+        {/* Forbidden Route */}
+        <Route
+          path="forbidden"
+          element={
+            <MainLayout>
+              <Forbidden />
+            </MainLayout>
+          }
+        />
 
-                {/* Forbidden Route */}
-                <Route
-                    path='forbidden'
-                    element={
-                        <MainLayout>
-                            <Forbidden />
-                        </MainLayout>
-                    }
-                />
-
-                {/* Catch-all route for undefined paths */}
-                <Route
-                    path='*'
-                    element={
-                        <MainLayout>
-                            <NotFound />
-                        </MainLayout>
-                    }
-                />
-            </Routes>
-        </Suspense>
-    );
+        {/* Catch-all route for undefined paths */}
+        <Route
+          path="*"
+          element={
+            <MainLayout>
+              <NotFound />
+            </MainLayout>
+          }
+        />
+      </Routes>
+    </Suspense>
+  );
 }
 
 export default AppRouter;
